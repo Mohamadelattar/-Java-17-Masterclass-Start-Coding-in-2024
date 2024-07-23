760c149f5a8c2232410636ec69587d89bda9c1d4
Mohamed El attar <mohamadelattar99@gmail.com>
2024-07-24 00:39:44 +0100

    TeenNumberChecker

---
+++ TeenNumberChecker.java
@@ -0,0 +1,15 @@
+public class TeenNumberChecker {
+    // write code here
+    public static boolean hasTeen(int first, int second, int third) {
+        return isTeen(first) || isTeen(second) || isTeen(third);
+
+
+    }
+
+    public static boolean isTeen(int first) {
+        return (first >= 13 && first <= 19);
+
+    }
+
+
+}
